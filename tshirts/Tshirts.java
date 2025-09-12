3a550fc7a20348c067910daacbd94273ef7aac6b
marco-banos <bma2que@bosch.com>
2025-09-12 08:24:30 -0600

    second commit
    second commit

---
+++ tshirts/Tshirts.java
@@ -0,0 +1,18 @@
+public class Tshirts {
+    static String size(int cms) {
+        if(cms < 38) {
+            return "S";
+        } else if(cms > 38 && cms < 42) {
+            return "M";
+        } else {
+            return "L";
+        }
+    }
+
+    public static void main(String[] args) { 
+        assert(size(37) == "S");
+        assert(size(40) == "M");
+        assert(size(43) == "L");
+        System.out.println("All is well (maybe!)");
+    }
+}
